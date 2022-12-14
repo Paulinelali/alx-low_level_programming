3d962edc882b84a6b5ebf801f0047c127c7f6e1d
Ayomide Sunday <ayomidesunday31@gmail.com>
2022-12-14 03:16:44 -0800

    fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,19 @@
  * print-alphabelt entry state
  * return 0; becuse of int used for main
  */
+
 void print_alphabet(void)
 {
 	char c = 'a';
 
 	while(c <= 'z')
 	{
+
 		_putchar(c);
 		c++;
 
+
 	}
+
 	_putchar('\n');
 }
