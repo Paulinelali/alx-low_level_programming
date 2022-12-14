8f1a89dec46e0aadebe5c5a68441a776a1fa799e
Ayomide Sunday <ayomidesunday31@gmail.com>
2022-12-14 04:10:38 -0800

    fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * print_alphabet_x10 - prints alphabet 10 times
- *   * Return:void
- *    */
+ * print_alphabet_x10 - prints alphabet 10 times
+* Return:void
+*/
 
 void print_alphabet_x10(void)
 {
