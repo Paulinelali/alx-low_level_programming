4f4e4a6806a97d1fa223013ec5e66587f993a770
Ayomide Sunday <ayomidesunday31@gmail.com>
2022-12-14 11:42:21 -0800

    mom

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,13 @@ void print_to_98(int n)
 
 	while (n < 98)
 	{
-			printf("%i, ", n);
-				n++;
+		printf("%i, ", n);
+			n++;
 	}
 	while (n > 98)
 	{
-			printf("%i, ", n);
-				n--;
+		printf("%i, ", n);
+			n--;
 	}
 
 	printf("98");
