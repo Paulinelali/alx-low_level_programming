24a08b8d1bb641fb133abbe98f76cde50c8f1f88
Ayomide Sunday <ayomidesunday31@gmail.com>
2022-12-14 04:22:03 -0800

    mo

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * _islower - check for lower case letter
- *   * @c : character to check the case
- *    * Return:0 or 1
- *     */
+ * _islower - check for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
 
 int _islower(int c)
 {
