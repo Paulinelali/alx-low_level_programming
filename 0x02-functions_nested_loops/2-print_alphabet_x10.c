5a544513d9119a6a93a2672f1ae11e999e51c948
Ayomide Sunday <ayomidesunday31@gmail.com>
2022-12-14 03:46:24 -0800

    fixing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,24 +7,25 @@
 
 void print_alphabet_x10( void )
 {
+
 	char c;
 	int i = 0;
 
-	while( i < 10 )
+	while(i < 10)
 
 	{
 		c = 'a';
 
-		while( c <= 'z')
+		while(c <= 'z')
 		{
 
-			_putchar( c );
+			_putchar(c);
 
 			c++;
 
 		}
 
-		_putchar( '\n' );
+		_putchar('\n');
 
 		i++;
 	}
