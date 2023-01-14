9fee04f73ec189ab831816cac1ab2e5650ff1611
Ayomide Sunday <ayomidesunday31@gmail.com>
2023-01-14 13:29:31 +0100

    Write a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -47,7 +47,6 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 		mulrem = mul / 10;
 		add = (dest[k] - '0') + (mul % 10) + addrem;
 		addrem = add / 10;
-
 		dest[k] = add % 10 + '0';
 	}
 	for (addrem += mulrem; k >= 0 && addrem; k--)
@@ -76,7 +75,6 @@ int check_for_digits(char **av)
 	{
 		for (j = 0; av[i][j]; j++)
 		{
-
 			if (av[i][j] < '0' || av[i][j] > '9')
 				return (1);
 		}
@@ -128,21 +126,21 @@ int main(int argc, char *argv[])
 	a = malloc(ln * sizeof(char));
 	if (a == NULL)
 	{
-	for (ti = 0; e[ti]; ti++)
-																				_putchar(e[ti]);
-	exit(98);
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
 	}
 	init(a, ln - 1);
 	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
 		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-		if (t == NULL)
-		{
-			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
-			free(a);
-			exit(98);
-		}
+			if (t == NULL)
+			{
+				for (ti = 0; e[ti]; ti++)
+					_putchar(e[ti]);
+				free(a);
+				exit(98);
+			}
 	}
 	_print(a, ln - 1);
 	return (0);
